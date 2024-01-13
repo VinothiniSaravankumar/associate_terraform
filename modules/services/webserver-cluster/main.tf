c67a636493d9c5b06e5dfb843cd710d486f023e0
VinothiniSaravankumar <vinothinithangam6@gmail.com>
2024-01-13 23:48:40 +0800

    module chapter

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -48,7 +48,7 @@ resource "aws_launch_configuration" "example" {
   #             nohup busybox httpd -f -p ${var.server_port} &
   #             EOF
 
-  user_data = templatefile("../../../modules/services/webserver-cluster/user-data.sh", {
+  user_data = templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
     db_address = data.terraform_remote_state.db.outputs.address
     db_port = data.terraform_remote_state.db.outputs.port
@@ -91,12 +91,22 @@ data "aws_subnets" "default" {
 resource "aws_security_group" "instance" {
   # name = "terraform-example-instance"
   name = "${var.cluster_name}-instance"
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+  # Changing the below inline config to separate config
+  # ingress {
+  #   from_port   = local.http_port
+  #   to_port     = local.http_port
+  #   protocol    = local.tcp_protocol
+  #   cidr_blocks = local.all_ips
+  # }
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb" "example" {
@@ -146,20 +156,39 @@ resource "aws_security_group" "alb" {
   # name = "terraform-example-alb"
   name = "${var.cluster_name}-alb"
   # Allow inbound HTTP requests
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+  ## Changing the inline config  to separate resources
+  # ingress {
+  #   from_port   = local.http_port
+  #   to_port     = local.http_port
+  #   protocol    = local.any_protocol
+  #   cidr_blocks = local.all_ips
+  # }
 
-  # Allow all outbound requests
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+  # # Allow all outbound requests
+  # egress {
+  #   from_port   = local.any_port
+  #   to_port     = local.any_port
+  #   protocol    = local.any_protocol
+  #   cidr_blocks = local.all_ips
+  # }
+}
+
+resource "aws_security_group_rule" "allow_http_inbound_alb" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound_alb" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_listener_rule" "asg" {
@@ -177,4 +206,6 @@ resource "aws_lb_listener_rule" "asg" {
     target_group_arn = aws_lb_target_group.asg.arn
   }
 }
+
+
 
